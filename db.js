4431e4fc570203bb618c512dbb0fbfb19e2c2d00
ovan <geoyansen@gmail.com>
2026-02-02 20:43:34 +0700

    revisi 4 deploy railway

--- db.js
+++ db.js
@@ -1,16 +1,8 @@
 const { Pool } = require('pg');
 
-// Tambahkan baris ini untuk "membersihkan" variabel yang bentrok dari Railway
-delete process.env.PGHOST;
-delete process.env.PGUSER;
-delete process.env.PGPASSWORD;
-delete process.env.PGDATABASE;
-delete process.env.PGPORT;
-
-console.log("Checking DATABASE_URL...", process.env.DATABASE_URL ? "Tersedia" : "TIDAK DITEMUKAN");
-
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  // Masukkan string koneksi Supabase Anda secara langsung di sini
+  connectionString: 'postgresql://postgres:#17Desember2003@db.fztgslkithvwksebvxok.supabase.co:5432/postgres',
   ssl: {
     rejectUnauthorized: false 
   }
